4190de0e1d36bf4fc8351558f7d308d6e084379b
Mehedi Hasan <developerhasan99@gmail.com>
2025-10-09 11:26:37 +0600

    Done home page

--- src/theme.js
+++ src/theme.js
@@ -7,3 +7,24 @@ import "./search.js";
 
 window.Alpine = Alpine;
 Alpine.start();
+
+// Homepage trending product slider
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".home_trending_slider", {
+    modules: [Navigation],
+    slidesPerView: 2,
+    spaceBetween: 30,
+    breakpoints: {
+      768: { slidesPerView: 3 },
+      1024: { slidesPerView: 4 },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
